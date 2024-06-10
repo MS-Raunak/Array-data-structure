54b79bb235f6f31c64d97d4e31517e649999697e
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-10 14:55:34 +0530

    **Selection Sorting(Descending) Technique

--- src/_1dArray/SortingAlgorithm/SelectionSort.java
+++ src/_1dArray/SortingAlgorithm/SelectionSort.java
@@ -3,6 +3,9 @@ package _1dArray.SortingAlgorithm;
 import java.util.Arrays;
 
 /**
+ * Implements the selection sort algorithm by iterating over the array,
+ * finding the smallest element, and swapping it with the first unsorted element.
+ *
  * Step-1: Initially, the entire array is considered as the unsorted sub-array,
  * and the sorted sub-array is empty.
  *
@@ -16,7 +19,7 @@ import java.util.Arrays;
  */
 public class SelectionSort {
 
-    //Selection Sort: Implements the selection sort algorithm by iterating over the array, finding the smallest element, and swapping it with the first unsorted element.
+    //Selection Sort in Ascending
     private static int[] selectionSort(int[] arr) {
         for (int i=0; i< arr.length; i++) {
             int smallestEleIdx=i;//select ith element
@@ -32,10 +35,28 @@ public class SelectionSort {
         }
         return arr;
     }
+
+    //Selection Sort in Ascending
+    private static int[] selectionSortDesc(int[] arr) {
+        for (int i=0; i< arr.length; i++) {
+            int smallestEleIdx=i;//select ith element
+            for (int j=i+1; j< arr.length; j++) {
+                //comparison b/w ith ele and all elements of right-side
+                if (arr[j]>arr[smallestEleIdx])
+                    smallestEleIdx = j;
+
+            }
+            int temp = arr[i];
+            arr[i] = arr[smallestEleIdx];
+            arr[smallestEleIdx] = temp;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         int[]arr = {20, 22, 13, 5, 97};
         System.out.println("Given Array: " + Arrays.toString(arr));
         System.out.println("Sorted Array: " + Arrays.toString(selectionSort(arr)));
+        System.out.println("Sorted Array: " + Arrays.toString(selectionSortDesc(arr)));
 
     }
 
