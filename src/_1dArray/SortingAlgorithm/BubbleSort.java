74d22d508f846a1a255dc13587bf874663c2a074
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-10 13:28:42 +0530

    **bubble sorting(in ascending) technique

---
+++ src/_1dArray/SortingAlgorithm/BubbleSort.java
@@ -0,0 +1,36 @@
+package _1dArray.SortingAlgorithm;
+
+import java.util.Arrays;
+
+/**
+ * Bubble Sorting:
+ * Easiest sorting technique where each every elements perform comparison from their
+ * right side element, if left side element is greater than right-side element the swap
+ * both element to each other
+ * Example:
+ * Given Array: [20, 22, 13, 5, 97]
+ * Sorted Array: [5, 13, 20, 22, 97]
+ */
+public class BubbleSort {
+    //Bubble Sort
+    private static int[] bubbleSort(int[] arr) {
+        for (int i=0; i< arr.length; i++) {
+            for (int j=i+1; j< arr.length; j++) {
+                if (arr[j]<arr[i]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[]arr = {20, 22, 13, 5, 97};
+        System.out.println("Given Array: " + Arrays.toString(arr));
+        System.out.println("Sorted Array: " + Arrays.toString(bubbleSort(arr)));
+
+    }
+
+}
