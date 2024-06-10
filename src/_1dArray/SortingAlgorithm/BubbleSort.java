ef1dad9e3b3d60fd15c0a5e62ae0735d55eef27d
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-10 14:57:27 +0530

    **Bubble Sorting(Descending) Technique

--- src/_1dArray/SortingAlgorithm/BubbleSort.java
+++ src/_1dArray/SortingAlgorithm/BubbleSort.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  * Sorted Array: [5, 13, 20, 22, 97]
  */
 public class BubbleSort {
-    //Bubble Sort
+    //Bubble Sort in Ascending
     private static int[] bubbleSort(int[] arr) {
         for (int i=0; i< arr.length; i++) {
             for (int j=i+1; j< arr.length; j++) {
@@ -26,10 +26,25 @@ public class BubbleSort {
         return arr;
     }
 
+    //Bubble Sort in Ascending
+    private static int[] bubbleSortDesc(int[] arr) {
+        for (int i=0; i< arr.length; i++) {
+            for (int j=i+1; j< arr.length; j++) {
+                if (arr[j]>arr[i]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         int[]arr = {20, 22, 13, 5, 97};
         System.out.println("Given Array: " + Arrays.toString(arr));
-        System.out.println("Sorted Array: " + Arrays.toString(bubbleSort(arr)));
+        System.out.println("Sorted Array(Ascending): " + Arrays.toString(bubbleSort(arr)));
+        System.out.println("Sorted Array(Descending): " + Arrays.toString(bubbleSortDesc(arr)));
 
     }
 
