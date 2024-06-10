851a8cf158d305c8991abe70ba0ec1ba368d65f7
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-10 14:58:39 +0530

    **Bubble Sorting(Descending) Technique

--- src/_1dArray/SortingAlgorithm/BubbleSort.java
+++ src/_1dArray/SortingAlgorithm/BubbleSort.java
@@ -26,7 +26,7 @@ public class BubbleSort {
         return arr;
     }
 
-    //Bubble Sort in Ascending
+    //Bubble Sort in Descending
     private static int[] bubbleSortDesc(int[] arr) {
         for (int i=0; i< arr.length; i++) {
             for (int j=i+1; j< arr.length; j++) {
